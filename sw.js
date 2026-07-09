8c0b8cdeef0cebfcf7753a9dafb5dc6df330ae6b
Harris-26 <1164251286@qq.com>
2026-07-09 15:11:50 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+﻿const CACHE = "winrate-v1";
+const URLS = [
+  "/盈亏平衡分析.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE; }).map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
